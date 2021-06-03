07ccaa5d0428fcba81fce5231ca7ec9e138b01c1
vamsijak <84320438+vamsijak@users.noreply.github.com>
2021-06-03 10:49:33 +0530

    Update kubectl.sh

--- lab/minikube-kubectl/kubectl.sh
+++ lab/minikube-kubectl/kubectl.sh
@@ -6,3 +6,9 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 minikube dashboard --url
 
+
+
+kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment web --type=NodePort --port=8080
+kubectl get service web
+minikube service web --url
