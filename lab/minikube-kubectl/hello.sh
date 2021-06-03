8a6f8d1f296b8f7e4088ee40edc27f4bcce26c28
vamsijak <84320438+vamsijak@users.noreply.github.com>
2021-06-03 10:47:03 +0530

    Create hello.sh

---
+++ lab/minikube-kubectl/hello.sh
@@ -0,0 +1,4 @@
+kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment web --type=NodePort --port=8080
+kubectl get service web
+minikube service web --url
